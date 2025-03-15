ff494494e6579cf3b9dec770193d888e7ab8dc22
JstnAxel <axelkoe06@gmail.com>
2025-03-15 14:08:41 +0700

    Product Listing

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -7,6 +7,7 @@ use App\Enums\RolesEnum;
 use App\Filament\Resources\ProductResource\Pages;
 use App\Filament\Resources\ProductResource\Pages\EditProduct;
 use App\Filament\Resources\ProductResource\Pages\ProductImages;
+use App\Filament\Resources\ProductResource\Pages\ProductVariations;
 use App\Filament\Resources\ProductResource\Pages\ProductVariationTypes;
 use App\Filament\Resources\ProductResource\RelationManagers;
 use App\Models\Product;
@@ -36,6 +37,11 @@ class ProductResource extends Resource
     protected static ?string $navigationIcon = 'heroicon-m-queue-list';
 
     protected static SubNavigationPosition $subNavigationPosition = SubNavigationPosition::End;
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()->forVendor();
+    }
 
     public static function form(Form $form): Form
     {
@@ -162,7 +168,8 @@ class ProductResource extends Resource
             'create' => Pages\CreateProduct::route('/create'),
             'edit' => Pages\EditProduct::route('/{record}/edit'),
             'images' => Pages\ProductImages::route('/{record}/images'),
-            'variation-types' => Pages\ProductVariationTypes::route('/{record}/variation-types')
+            'variation-types' => Pages\ProductVariationTypes::route('/{record}/variation-types'),
+            'variations' => Pages\ProductVariations::route('/{record}/variations')
         ];
     }
 
@@ -172,6 +179,7 @@ class ProductResource extends Resource
                 EditProduct::class,
                 ProductImages::class,
                 ProductVariationTypes::class,
+                ProductVariations::class,
             ]);
 
     }
---
+++ resources/js/Components/Core/CurrencyFormatter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CurrencyFormatter(
+  {amount, currency= 'IDR', locale}: {amount:number, currency?:string, locale?: string}) {
+  return new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency
+  }).format(amount)
+}
+
+export default CurrencyFormatter;
